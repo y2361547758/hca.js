57aeda54632ed3089646dd7dbd95475be6c82f2d
segfault-bilibili <segfault_bilibili@qq.com>
2022-05-10 12:42:14 +0800

    mark standalone version as right-click save-able

--- generate-standalone.sh
+++ generate-standalone.sh
@@ -8,3 +8,4 @@ grep -A999999 'script src="hca.js"' hca.html | grep -v 'script src="hca.js"' | \
 grep -B999999 ' const hcaJsUrl ' | grep -v ' const hcaJsUrl ' >> hca-standalone.html
 echo -ne "        const hcaJsUrl = new URL(URL.createObjectURL(new Blob([document.querySelector(\"#hcajs\").textContent], {type: \"text/javascript\"})));\n" >> hca-standalone.html
 grep -A999999 ' const hcaJsUrl ' hca.html | grep -v ' const hcaJsUrl ' >> hca-standalone.html
+sed -i '/<h1>HCA decoder demo<\/h1>/a\    <i>Standalone version - you may right-click & save this page for offline use.<\/i><br>' hca-standalone.html
